032c630b765c022e3c96902a5c240a24f517ed13
bharati1996 <109621550+bharati1996@users.noreply.github.com>
2022-09-15 00:01:46 +0530

    Add files via upload

---
+++ DSA.problem4.py
@@ -0,0 +1,12 @@
+#Write a program to print the first non-repeated character from the string.
+
+def isrepeated(w1):
+    w3=""
+    for i in w1:
+        if i not in w3:
+            w3=w3+i
+    print(w3)
+a="Bharati"
+isrepeated(a)
+
+
